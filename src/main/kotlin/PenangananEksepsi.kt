ab6598ec4ef49e53f26e9df71e7a36e8f8eeb08a
Kurniawan Hamdi <kurniawanhamdi99@gmail.com>
2022-03-17 14:32:11 +0700

    contoh kode program menggunakan penanganan eksepsi di kotlin

---
+++ src/main/kotlin/PenangananEksepsi.kt
@@ -0,0 +1,15 @@
+//Penanganan eksepsi adalah bagian penting dari bahasa pemrograman. Teknik ini menghalangi
+//aplikasi kita memberikan output yang salah saat runtime. Penggunaan eksepsi di Kotlin sama
+//seperti di Java. Semua eksepsi adalah turunan dari kelas “Throwable”. Berikut adalah contoh
+//kode program penanganan eksepsi:
+fun main(args: Array<String>) {
+    try {
+        val myVar:Int = 10;
+        val v:String = "Rekayasa Perangkat Lunak";
+        v.toInt();
+    } catch(e:Exception) {
+        e.printStackTrace()
+    } finally {
+        println("Exception Handeling in Kotlin");
+    }
+}
